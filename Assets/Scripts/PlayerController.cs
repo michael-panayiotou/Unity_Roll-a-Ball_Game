5d739c984ec161566e9f116eabfc5fc837cbad32
Michael Panayiotou <michael@CUTings-MacBook-Pro.local>
2015-03-28 02:07:56 +0200

    final commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	//public variables
+	public float speed;
+	private int count;
+	public GUIText countText;
+
+	void Start()
+	{
+		count = 0;
+		SetCountText();
+	}
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+	}
+	void FixedUpdate()
+	{
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		GetComponent<Rigidbody>().AddForce(movement * speed *Time.deltaTime);
+
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+
+		if(other.gameObject.tag == "Pickup")
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+
+		}
+
+		//Destroy(other.gameObject);
+	}
+
+}
+
+
